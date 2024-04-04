abd541db24e6ccf14e06c5719eda9da32c1587c8
JeremyCitrawira <jeremycitrawira@u.nus.edu>
2024-04-04 12:17:41 +0800

    Layout Finished (with dummy API calls using Flask)

--- KPI_Summary/dummy.py
+++ KPI_Summary/dummy.py
@@ -8,10 +8,19 @@ CORS(app)
 def get_data():
     # Your code to fetch and process data from the desired source
     # For simplicity, let's assume dummy data
-    data = [{'date': '2024-01-01', 'value': 10},
-            {'date': '2024-01-02', 'value': 20},
-            {'date': '2024-01-03', 'value': 15}]
+    data = [{'date': '2024-01-01', 'value': 10, 'account': 25},
+            {'date': '2024-01-02', 'value': 20, 'account':50},
+            {'date': '2024-01-03', 'value': 15, 'account':60}]
     return jsonify(data)
 
+@app.route('/api/model')
+def get_model():
+    # Your code to fetch and process data from the desired source
+    # For simplicity, let's assume dummy data
+    model_perform = [{'accuracy':{'positive': '61%', 'negative':'72%','average':'65%'}
+             ,'precision':{'positive': '61%', 'negative':'72%','average':'65%'}
+             ,'recall':{'positive': '61%', 'negative':'72%','average':'65%'}
+             ,'f1':{'positive': '61%', 'negative':'72%','average':'65%'}}]
+    return jsonify(model_perform)
 if __name__ == '__main__':
     app.run(debug=True)
--- KPI_Summary/script.js
+++ KPI_Summary/script.js
@@ -4,6 +4,30 @@ $(document).ready(function() {
     var minStartDate = new Date('2023-01-01')
     var maxEndDate = new Date('2023-12-31')
 
+    // Function to populate the table with JSON data
+    function populateTable(data) {
+        // Assuming your JSON data is in the format of dictionary of dictionaries
+        // Iterate over the outer dictionary
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                var innerData = data[key];
+                for (var each in innerData){
+                    if (innerData.hasOwnProperty(each)){
+                        var cell = innerData[each];
+                        let string =each;
+                        for (var indiv in cell){
+                            if (cell.hasOwnProperty(indiv)){
+                                var final = cell[indiv];
+                                let id =string.concat("-",indiv);
+                                document.getElementById(id).innerHTML = final;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+   
     // Initialize datepicker for start date
     $('#start-date').datepicker({
         dateFormat: 'dd/mm/yy', // Set the date format to dd/mm/yy
@@ -22,6 +46,7 @@ $(document).ready(function() {
         maxDate: maxEndDate,
         onSelect: function(selectedDate) {
             endDate = selectedDate;
+            $('#start-date').datepicker('option', 'maxDate', selectedDate)
         }
     });
   
@@ -52,25 +77,120 @@ $(document).ready(function() {
         // Process data for visualization
         const dates = data.map(entry => entry.date);
         const values = data.map(entry => entry.value);
+        const account =data.map(entry => entry.account);
+
+        const neg_account = account.map(function(x) {return x*-1;});
 
         // Plot data using Plotly
         const trace = {
             x: dates,
             y: values,
-            type: 'scatter'
+            type: 'scatter',
+            mode:'lines',
+            line: {color: 'rgb(159,6,6)'}
         };
+
+        const trace2 ={
+            x: dates,
+            y: neg_account,
+            type: 'scatter',
+            mode:'lines',
+            line: {color: 'rgb(159,6,6)'}
+        }
+        var config = {
+            responsive: true,
+        }
         const layout = {
-            title: 'Data Visualization',
+            title: {
+                automargin:false,
+                yref:"container",
+                yanchor:"center",
+                text: "Historical Churn Occurrences",
+                font:{
+                    family: 'Inter',
+                    size: 18,
+                    color:"#808080"
+                }
+            },
             xaxis: {
-                title: 'Date'
+                title: {
+                    text: '<b> Date </b>',
+                    font:{
+                        family: 'Inter',
+                        size: 10
+                    }
+                },
+                font:{
+                    family: 'Inter',
+                    size: 10
+                }
             },
             yaxis: {
-                title: 'Value'
-            }
+                title: {
+                    text: '<b> # of Churn Occurrences </b>',
+                    font:{
+                        family: 'Inter',
+                        size: 10
+                    }
+                },
+                font:{
+                    family: 'Inter',
+                    size: 10
+                }
+            },
+            plot_bgcolor: "#ffffff00",
+            paper_bgcolor:"#ffffff00"
         };
-        Plotly.newPlot('graph', [trace], layout);
+        
+        const layout2 = {
+            title: {
+                automargin:false,
+                yref:"container",
+                yanchor:"center",
+                text: "Raw Loss Impact",
+                font:{
+                    family: 'Inter',
+                    size: 18,
+                    color:"#808080"
+                }
+            },
+            xaxis: {
+                title: {
+                    text: '<b> Date </b>',
+                    font:{
+                        family: 'Inter',
+                        size: 10
+                    }
+                },
+                font:{
+                    family: 'Inter',
+                    size: 10
+                }
+            },
+            yaxis: {
+                title: {
+                    text: '<b> Net Loss </b>',
+                    font:{
+                        family: 'Inter',
+                        size: 10
+                    }
+                },
+                font:{
+                    family: 'Inter',
+                    size: 10
+                }
+            },
+            plot_bgcolor: "#ffffff00",
+            paper_bgcolor:"#ffffff00"
+        };
+        Plotly.newPlot('graph', [trace], layout,config);
+        Plotly.newPlot('graph2',[trace2], layout2,config);
     })
+    $.getJSON('http://127.0.0.1:5000/api/model', function(data) {
+        populateTable(data);
+    })  
     .fail(function(error) {
         console.error('Error fetching data:', error);
     });
 });
+
