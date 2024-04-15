eaf5d6189eb2b5c2a8051d6aeb643328e1db5a9a
Hera <hastherah@gmail.com>
2024-04-15 16:42:09 +0800

    Adding BackendOutput.py and Report_Dict.json

---
+++ BackendOutput.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+import pandas as pd
+from flask import Flask, jsonify
+from flask_cors import CORS
+from datetime import datetime
+import json
+
+
+# ### Uplading predicted data & report dictionary
+
+# In[39]:
+
+
+predicted_data = pd.read_csv('test.csv')
+
+
+# In[40]:
+
+
+with open('Report_Dict.json', 'r') as file:
+        report_dict = json.load(file)
+
+
+# ### Creating string of Last Updated
+
+# In[41]:
+
+
+LastUpdated = {'LastUpdated' : f"Last Updated at {datetime.now().replace(microsecond=0)}"}
+jsonLastUpdated = json.dumps(LastUpdated)
+
+
+# ### Sending data
+
+# In[42]:
+
+
+# Flask API to send report and predicted data to frontend
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/model')
+def get_model():
+    return report_dict
+
+@app.route('/api/data')
+def get_data():
+    return jsonify(predicted_data.to_dict(orient='records'))
+
+if __name__ == 'main':
+    app.run(debug=True)
+
+
+
+
