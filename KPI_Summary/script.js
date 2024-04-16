ba2a918c0a64b003655e21d29785440e71397feb
JeremyCitrawira <jcitrawira@gmail.com>
2024-04-16 18:08:35 +0800

    changed codebase to python backend + adjusted to API JSON formatting

--- KPI_Summary/script.js
+++ KPI_Summary/script.js
@@ -1,20 +1,49 @@
 $(document).ready(function() {
+    $('body').append('<div id="spinner-bg" class="spinner-bg"><img src="logo.png" alt="GxSmartChurn logo"><p>Running...</p><div id="spinner" class="spinner"></div></div>');
+    document.getElementById('spinner-bg').classList.remove('hidden')
     var startDate = null;
     var endDate = null;
-    var pastSixMonths =false;
-    var pastOneYear = false;
-    var customDate = false;
-    var minStartDate = new Date('2024-01-01')
-    var maxEndDate = new Date('2024-12-31')
-    var DataFrame = dfjs.DataFrame;
-    var rawDf = null;
+    var minStartDate = new Date("2022-11-29");
+    var maxEndDate = new Date("2024-04-13");
 
+
+    function process(data){
+        document.getElementById("hist-churn").innerHTML = data["hist-churn"]
+        document.getElementById("proj-churn").innerHTML = data["proj-churn"]
+        const hist_churn = data["hist-churn-graph"]
+        const loss_impact = data["loss-impact-graph"]
+        var figure = JSON.parse(hist_churn);
+        Plotly.newPlot('graph', figure.data, figure.layout);
+        var figure2 = JSON.parse(loss_impact);
+        Plotly.newPlot('graph2', figure2.data, figure2.layout);
+        document.getElementById("first-persona-main").innerHTML = data["persona-segment-top-3"][0]["persona"]
+        document.getElementById("first-percent-main").innerHTML = data["persona-segment-top-3"][0]["pctg"]
+        document.getElementById("second-persona-main").innerHTML = data["persona-segment-top-3"][1]["persona"]
+        document.getElementById("second-percent-main").innerHTML = data["persona-segment-top-3"][1]["pctg"]
+        document.getElementById("third-persona-main").innerHTML = data["persona-segment-top-3"][2]["persona"]
+        document.getElementById("third-percent-main").innerHTML = data["persona-segment-top-3"][2]["pctg"]
+
+        document.getElementById("first-persona").innerHTML = data["persona-segment-top-3"][0]["persona"]
+        document.getElementById("first-percent").innerHTML = data["persona-segment-top-3"][0]["pctg"]
+        document.getElementById("second-persona").innerHTML = data["persona-segment-top-3"][1]["persona"]
+        document.getElementById("second-percent").innerHTML = data["persona-segment-top-3"][1]["pctg"]
+        document.getElementById("third-persona").innerHTML = data["persona-segment-top-3"][2]["persona"]
+        document.getElementById("third-percent").innerHTML = data["persona-segment-top-3"][2]["pctg"]
+        document.getElementById("fourth-persona").innerHTML = data["persona-segment-full"][3]["persona"]
+        document.getElementById("fourth-percent").innerHTML = data["persona-segment-full"][3]["pctg"]
+        document.getElementById("fifth-persona").innerHTML = data["persona-segment-full"][4]["persona"]
+        document.getElementById("fifth-percent").innerHTML = data["persona-segment-full"][4]["pctg"]
+        document.getElementById("sixth-persona").innerHTML = data["persona-segment-full"][5]["persona"]
+        document.getElementById("sixth-percent").innerHTML = data["persona-segment-full"][5]["pctg"]
+        document.getElementById("seventh-persona").innerHTML = data["persona-segment-full"][6]["persona"]
+        document.getElementById("seventh-percent").innerHTML = data["persona-segment-full"][6]["pctg"]
+        document.getElementById("timestamp").innerHTML = data["timestamp"]
+    }
     // Function to populate the table with JSON data
     function populateTable(data) {
         // Assuming your JSON data is in the format of dictionary of dictionaries
         // Iterate over the outer dictionary
         for (var key in data) {
-            console.log(key); //debugging
             if (key == 'weighted avg'){
                 continue;
             }
@@ -33,7 +62,6 @@ $(document).ready(function() {
                 classify ='average';
             }
             var innerData = data[key];
-            console.log(innerData);
             for (var each in innerData){
                 var cell = innerData[each];
                 if (each == 'support'){
@@ -48,243 +76,7 @@ $(document).ready(function() {
                 
             }
         }
-    }
-
-    function filtered(rawDf){
-        var currDate = new Date(Date.now());
-        if (!pastOneYear && !pastSixMonths && !customDate){
-            return rawDf;
-        }
-        else if (pastOneYear){
-            var lastYear = new Date(currDate);
-            lastYear = new Date(lastYear.setFullYear(lastYear.getFullYear()-1));
-            var newDf = rawDf.filter(row => {var RowDate= new Date(row.get('date')); return RowDate.getTime() <= currDate.getTime() && RowDate.getTime() >= lastYear.getTime()});
-            console.log(newDf.show());
-            return newDf;
-        }
-        else if (pastSixMonths){
-            var lastSixMonths = new Date(currDate);
-            lastSixMonths.setMonth(lastSixMonths.getMonth()-6);
-            var newDf = rawDf.filter(row =>{var RowDate= new Date(row.get('date')); return RowDate.getTime() <= currDate.getTime() && RowDate.getTime() >= lastSixMonths.getTime()});
-            console.log(newDf.show());
-            return newDf;
-        }
-        else if(customDate){
-            var start = new Date(startDate);
-            console.log(start);
-            var end = new Date(endDate);
-            console.log(end);
-            var newDf = rawDf.filter(row => {var RowDate= row.get('date'); console.log(RowDate); return new Date(RowDate).getTime() <= end.getTime() && new Date(RowDate).getTime() >= start.getTime()});
-            console.log(newDf.show());
-            return newDf;
-        }
-    }
-    function persona_segment(persona){
-        let lengthOfArr = persona.length;
-        let frequencyTable = {};
-
-        // Count frequencies of each element
-        persona.forEach(element => {
-            frequencyTable[element] = (frequencyTable[element] || 0) + 1;
-        });
-    
-        // Convert object to array of [key, value] pairs
-        let frequencyArray = Object.entries(frequencyTable);
-    
-        // Sort the array based on the frequency in descending order
-        frequencyArray.sort((a, b) => b[1] - a[1]);
-        
-        let order = ['first','second','third']
-        var k = 0;
-        var complete = false;
-        var price = false;
-        var feature = false;
-        var support = false;
-        frequencyArray.forEach(array => {
-            let id = order[k]
-            let persona_name = array[0];
-            if (!complete){
-                if (persona_name == 'Price-Sensitive'){
-                    price = true;
-                }
-                else if (persona_name == 'Feature-Driven'){
-                    feature=true;
-                }
-                else{
-                    support=true;
-                }
-                if (price && feature && support){
-                    complete = true;
-                }
-            }
-            var persona_id = id.concat('-persona');
-            document.getElementById(persona_id).innerHTML = persona_name;
-            
-            let persona_freq = array[1];
-            let persona_pctg = ((persona_freq/lengthOfArr)*100).toFixed(1) + '%';
-            document.getElementById(id.concat('-percent')).innerHTML = persona_pctg;
-            k += 1
-    })
-        if (!complete){
-            if(!price){
-                let id = order[k];
-                var persona_id = id.concat('-persona');
-                document.getElementById(persona_id).innerHTML = "Price-Sensitive";
-                let persona_pctg = (0/100).toFixed(1) + '%';
-                document.getElementById(id.concat('-percent')).innerHTML = persona_pctg;
-                k +=1;
-            }
-            if(!feature){
-                let id = order[k];
-                var persona_id = id.concat('-persona');
-                document.getElementById(persona_id).innerHTML = "Feature-Driven";
-                let persona_pctg = (0/100).toFixed(1) + '%';
-                document.getElementById(id.concat('-percent')).innerHTML = persona_pctg;
-                k +=1;
-            }
-            if(!support){
-                let id = order[k];
-                var persona_id = id.concat('-persona');
-                document.getElementById(persona_id).innerHTML = "Support-Dependent";
-                let persona_pctg = (0/100).toFixed(1) + '%';
-                document.getElementById(id.concat('-percent')).innerHTML = persona_pctg;
-                k +=1;
-            } 
-        }
-    }
-    function updateData(rawDf){    
-        var df = filtered(rawDf);
-
-        // Process data for visualization
-        const dates = df.select('date').toArray().flat();
-        const account =df.select('account').toArray().flat();
-
-        const projected = df.select('churn_predict').toArray().flat();
-        let filtered_proj = projected.filter(num => num != null && num != NaN);
-        const average = array => array.reduce((a, b) => a + b) / array.length;
-        if (filtered_proj.length == 0){
-            document.getElementById("proj-churn").innerHTML = (0/100).toFixed(1) +'%';
-
-        }
-        else{        
-            let proj_churn_pctg = (average(filtered_proj)*100).toFixed(1) + '%'
-            document.getElementById("proj-churn").innerHTML = proj_churn_pctg;
-        }        
-        const persona = df.select('persona').toArray().flat();
-        persona_segment(persona);
-
-        const churn = df.select('churn').toArray().flat();
-        let churn_pctg = (average(churn)*100).toFixed(1) + '%'
-        document.getElementById("hist-churn").innerHTML = churn_pctg;
-
-        const neg_account = account.map(function(x) {return x*-1;});
-
-        // Plot data using Plotly
-        const trace = {
-            x: dates,
-            y: churn,
-            type: 'scatter',
-            mode:'lines+markers',
-            line: {color: 'rgb(159,6,6)'}
-        };
-
-        const trace2 ={
-            x: dates,
-            y: neg_account,
-            type: 'scatter',
-            mode:'lines+markers',
-            line: {color: 'rgb(159,6,6)'}
-        }
-        var config = {
-            responsive: true,
-        }
-        const layout = {
-            title: {
-                automargin:false,
-                yref:"container",
-                yanchor:"center",
-                text: "Historical Churn Occurrences",
-                font:{
-                    family: 'Inter',
-                    size: 18,
-                    color:"#808080"
-                }
-            },
-            xaxis: {
-                title: {
-                    text: '<b> Date </b>',
-                    font:{
-                        family: 'Inter',
-                        size: 10
-                    }
-                },
-                font:{
-                    family: 'Inter',
-                    size: 10
-                }
-            },
-            yaxis: {
-                title: {
-                    text: '<b> # of Churn Occurrences </b>',
-                    font:{
-                        family: 'Inter',
-                        size: 10
-                    }
-                },
-                font:{
-                    family: 'Inter',
-                    size: 10
-                }
-            },
-            plot_bgcolor: "#ffffff00",
-            paper_bgcolor:"#ffffff00"
-        };
-        
-        const layout2 = {
-            title: {
-                automargin:false,
-                yref:"container",
-                yanchor:"center",
-                text: "Raw Loss Impact",
-                font:{
-                    family: 'Inter',
-                    size: 18,
-                    color:"#808080"
-                }
-            },
-            xaxis: {
-                title: {
-                    text: '<b> Date </b>',
-                    font:{
-                        family: 'Inter',
-                        size: 10
-                    }
-                },
-                font:{
-                    family: 'Inter',
-                    size: 10
-                }
-            },
-            yaxis: {
-                title: {
-                    text: '<b> Net Loss </b>',
-                    font:{
-                        family: 'Inter',
-                        size: 10
-                    }
-                },
-                font:{
-                    family: 'Inter',
-                    size: 10
-                }
-            },
-            plot_bgcolor: "#ffffff00",
-            paper_bgcolor:"#ffffff00"
-        };
-        Plotly.newPlot('graph', [trace], layout,config);
-        Plotly.newPlot('graph2',[trace2], layout2,config);
-    }
-    
+    }  
     // Initialize datepicker for start date
     $('#start-date').datepicker({
         dateFormat: 'yy-mm-dd', // Set the date format to dd/mm/yy
@@ -316,38 +108,89 @@ $(document).ready(function() {
   
     // Fixed option 1 click event
     $('#past-six-month').click(function() {
-      console.log('Past 6 Months selected');
-      pastOneYear = false;
-      customDate = false;
-      pastSixMonths = true;
-      updateData(rawDf);
+        $('body').append('<div id="spinner-bg" class="spinner-bg"><img src="logo.png" alt="GxSmartChurn logo"><p>Running...</p><div id="spinner" class="spinner"></div></div>');
+        document.getElementById('spinner-bg').classList.remove('hidden')
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/api/time-filter/KPI-Summary');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText)
+                $.getJSON('http://127.0.0.1:5000/api/data/KPI-Summary', function(data) {
+                    process(data);
+                    document.getElementById("spinner-bg").style.opacity = "0";
+                    setTimeout(() => {
+                        // Code to be executed after 0.5 seconds
+                        document.getElementById("spinner-bg").remove();
+        }, 500);
+                })
+            } else {
+                console.log('Request failed. Status: ' + xhr.status);
+            }
+        };
+        xhr.send(JSON.stringify({ filter: 'past6months' }));
       $('.dropdown-content').hide();
     });
   
     // Fixed option 2 click event
     $('#past-one-year').click(function() {
-      console.log('Past 1 Year selected');
-      customDate = false;
-      pastSixMonths = false;
-      pastOneYear = true;
-      updateData(rawDf);
+        $('body').append('<div id="spinner-bg" class="spinner-bg"><img src="logo.png" alt="GxSmartChurn logo"><p>Running...</p><div id="spinner" class="spinner"></div></div>');
+        document.getElementById('spinner-bg').classList.remove('hidden')
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/api/time-filter/KPI-Summary');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText)
+                $.getJSON('http://127.0.0.1:5000/api/data/KPI-Summary', function(data) {
+                    process(data);
+                    document.getElementById("spinner-bg").style.opacity = "0";
+                    setTimeout(() => {
+                        // Code to be executed after 0.5 seconds
+                        document.getElementById("spinner-bg").remove();
+        }, 500);
+                })
+            } else {
+                console.log('Request failed. Status: ' + xhr.status);
+            }
+        };
+        xhr.send(JSON.stringify({ filter: 'past1year' }));
       $('.dropdown-content').hide();
     });
   
     // Apply button click event
     $('#filter-button').click(function() {
-        pastSixMonths = false;
-        pastOneYear = false;
-        customDate = true;
+        $('body').append('<div id="spinner-bg" class="spinner-bg"><img src="logo.png" alt="GxSmartChurn logo"><p>Running...</p><div id="spinner" class="spinner"></div></div>');
+        document.getElementById('spinner-bg').classList.remove('hidden')
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/api/time-filter/KPI-Summary');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText)
+                $.getJSON('http://127.0.0.1:5000/api/data/KPI-Summary', function(data) {
+                    process(data);
+                    document.getElementById("spinner-bg").style.opacity = "0";
+                    setTimeout(() => {
+                        // Code to be executed after 0.5 seconds
+                        document.getElementById("spinner-bg").remove();
+        }, 500);
+                })
+            } else {
+                console.log('Request failed. Status: ' + xhr.status);
+            }
+        };
+        xhr.send(JSON.stringify({ filter: 'customrange', start:startDate , end:endDate}));
         console.log('Selected date range:', startDate, 'to', endDate);
-        updateData(rawDf);
-      // Add your filtering logic here
     });
 
-    $.getJSON('http://127.0.0.1:5000/api/data', function(data) {
-        rawDf = new DataFrame(data);
-        console.log(rawDf.show())
-        updateData(rawDf);
+    $.getJSON('http://127.0.0.1:5000/api/data/KPI-Summary', function(data) {
+        process(data);
+        document.getElementById("spinner-bg").style.opacity = "0";
+        setTimeout(() => {
+            // Code to be executed after 0.5 seconds
+            document.getElementById("spinner-bg").remove();
+        }, 500);
     })
     $.getJSON('http://127.0.0.1:5000/api/model', function(data) {
         populateTable(data);
